ed2a317bf53e567222a0028f57f09d594bc4ad9b
joao <joaogabrieltarginosa@gmail.com>
2022-04-27 16:41:48 -0300

    começando a mexer com os templates

--- meu_site/settings.py
+++ meu_site/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -111,6 +112,11 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_REDIRECT_URL = 'home'
 
 
 # Static files (CSS, JavaScript, Images)
